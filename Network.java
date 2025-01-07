bb97ac72dd2a04f5e5342af8e53e89484268af1b
Tehila77 <t.alaloof77@gmail.com>
2025-01-07 22:29:36 +0200

    try fixed

--- Network.java
+++ Network.java
@@ -91,7 +91,10 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if(name1.equals(name2)|| name2==null||name1==null){
+        if(name2==null||name1==null){
+            return false;
+        }
+        if(name1.equals(name2)){
             return false;
         }
         if(getUser(name1)== null ||getUser(name2)== null){
